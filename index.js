b2a1a86af5f9e99bb26145eb06f83c39cc7f0c5e
Shubham Rahangdale <programmershubham755@gmail.com>
2025-09-03 16:19:17 +0530

    16:19

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 http.createServer((req, resp) => {
-    resp.write("hello");
+    resp.writeHead(200, {'Content-Type': 'text/html'});
+    resp.write("<h1>Hello from Node.js Server!</h1>");
     resp.end();
 }).listen(8080, () => {
     console.log("Server running on http://localhost:8080");
